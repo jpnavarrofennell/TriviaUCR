9e55ff65a4289274aab9a279bef558a7429c9262
Pablo Alfaro <pablo19512@gmail.com>
2021-09-04 10:46:01 -0600

    Imagen
    Creamos una nueva imagen, codigo == Sprite

---
+++ Assets/Scripts/CargadorImagenes.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public partial class Pregunta
+{
+    public Sprite imagenComplementaria;
+}
+public class CargadorImagenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
