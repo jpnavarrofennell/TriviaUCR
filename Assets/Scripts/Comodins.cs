fb6d8270f1a12593158d7dbea9125aba1ebd5673
CrisFX5045 <70766389+CrisFX5045@users.noreply.github.com>
2021-09-07 18:40:57 -0600

    Borramos lineas imnecesariass
    ok!

--- Assets/Scripts/Comodins.cs
+++ Assets/Scripts/Comodins.cs
@@ -12,16 +12,7 @@ public class Comodins : MonoBehaviour
 
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
+   
 
 
     //Inicio Seleccionar comodin.
